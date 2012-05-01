7f1f3a9fec488d3e36cdafac605afe8f51292c15
Cody Duval <cody.duval@gmail.com>
2012-05-01 16:04:39 -0400

    initial commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Houselistings2::Application.config.secret_token = '37bd1d83ba57d7cbb40ebb70414dbdb0289fdc183816631bb3f4c8c13df3b9fb1cba6b563d47dfe0f3d8738250281d7f5c7e696ddb42335087bf90b00dda2fc9'
