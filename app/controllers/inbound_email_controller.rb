aa0a3f029517f971038467273c17094e6e857f99
Cody Duval <cody.duval@gmail.com>
2012-05-16 15:24:43 -0400

    Updated emamil parser code

--- app/controllers/inbound_email_controller.rb
+++ app/controllers/inbound_email_controller.rb
@@ -2,21 +2,15 @@ class InboundEmailController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    Listing.create_from_postmark(Postmark::Mitt.new(request.body.read))
-    if @listing.save
-      Resque.enqueue(FetchListingData, @listing.id)
-      redirect_to @listing, :notice => "Successfully created new listing."
-    else
-      render 'new'
-    end
-    # @listing = Listing.new(params[:listing])
-    # if @listing.save
-    #   Resque.enqueue(FetchListingData, @listing.id)
-    #   redirect_to @listing, :notice => "Successfully created new listing."
-    # else
-    #   render 'new'
-    # end
+    @listing = Listing.create_from_postmark(Postmark::Mitt.new(request.body.read))
 
-    render :text => "Created a Listing!", :status => :created
+    if @listing.url
+      @listing.save
+      Resque.enqueue(FetchListingData, @listing.id)
+      render :text => "Created a Listing!", :status => :created
+    else
+      render :text => "Didn't Create a Listing!", :status => :created
+    end
+
   end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,14 +4,20 @@ class Listing < ActiveRecord::Base
   has_many :notes
   has_many :photos
 
+  validates :url, :presence => true
+
   def self.create_from_postmark(mitt)
     listing = Listing.new
     message_body = mitt.text_body
     parsed_uri_array = URI.extract(message_body, "http")
-    parsed_uri_messy = parsed_uri_array[0].to_s
-    parsed_uri = parsed_uri_messy.gsub(/[*]/,'')
-    listing.url = parsed_uri
-    listing.save
+    if parsed_uri_array.present?
+      parsed_uri_messy = parsed_uri_array[0].to_s
+      parsed_uri = parsed_uri_messy.gsub(/[*]/,'')
+      listing.url = parsed_uri
+      return listing
+    else
+      return listing
+    end
   end
   
 end
